2eac15895351f514cba95b53e5cbda29daecc15f
Prabin Giri <prabin17np@gmail.com>
2025-12-15 19:52:01 +0545

    adding about screen

--- lib/screens/bottom_screens/about_screen.dart
+++ lib/screens/bottom_screens/about_screen.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class AboutScreen extends StatefulWidget {
+  const AboutScreen({super.key});
+
+  @override
+  State<AboutScreen> createState() => _AboutScreenState();
+}
+
+class _AboutScreenState extends State<AboutScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(child: Center(child: Text("About Screen")));
+  }
+}
