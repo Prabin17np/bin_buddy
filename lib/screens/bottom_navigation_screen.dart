72de03e813804799900342bba13fca28de01060c
Prabin Giri <prabin17np@gmail.com>
2025-12-15 19:54:43 +0545

    bottom navigation screen

--- lib/screens/bottom_navigation_screen.dart
+++ lib/screens/bottom_navigation_screen.dart
@@ -1,1 +1,52 @@
+import 'package:bin_buddy/screens/bottom_screens/about_screen.dart';
+import 'package:bin_buddy/screens/bottom_screens/home_bottom_screen.dart';
+import 'package:bin_buddy/screens/bottom_screens/scan_bottom_screen.dart';
+import 'package:bin_buddy/screens/bottom_screens/search_bottom_screen.dart';
+import 'package:flutter/material.dart';
 
+class BottomNavigationScreen extends StatefulWidget {
+  const BottomNavigationScreen({super.key});
+
+  @override
+  State<BottomNavigationScreen> createState() => _BottomNavigationScreenState();
+}
+
+class _BottomNavigationScreenState extends State<BottomNavigationScreen> {
+  int _selectedIndex = 0;
+
+  List<Widget> lstBottomScreen = [
+    const HomeBottomScreen(),
+    const ScanBottomScreen(),
+    const SearchBottomScreen(),
+    const AboutScreen(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.scanner), label: "Scan"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+          BottomNavigationBarItem(icon: Icon(Icons.info), label: "About"),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.blueGrey,
+      ),
+      body: lstBottomScreen[_selectedIndex],
+    );
+  }
+}
