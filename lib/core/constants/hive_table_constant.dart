a46ebf46bfb1ee238b7f9b4c1eeb31c0d77956d3
Prabin Giri <prabin17np@gmail.com>
2026-01-01 18:52:51 +0545

    modify hive_table_constant

--- lib/core/constants/hive_table_constant.dart
+++ lib/core/constants/hive_table_constant.dart
@@ -1,1 +1,24 @@
+class HiveTableConstant {
+  HiveTableConstant._();
 
+  // Database Name
+  static const String dbName = 'binbuddy_database';
+
+  // Table Type IDs
+  static const int userTypeId = 0;
+  static const String userTable = 'user_table';
+
+  static const int recyclingProgressTypeId = 1;
+  static const String recyclingProgressTable = 'recycling_progress_table';
+
+  // Recycling Categories
+  static const String plasticCategory = "Plastic";
+  static const String paperCategory = "Paper";
+  static const String organicCategory = "Organic";
+  static const String metalCategory = "Metal";
+  static const String glassCategory = "Glass";
+  static const String electronicsCategory = "Electronics";
+
+  // Default User Role
+  static const String customerRole = "Customer";
+}
