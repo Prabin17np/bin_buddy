78887ca2f66737006250d45404d4d25f91371cd4
Prabin Giri <prabin17np@gmail.com>
2026-01-02 09:59:40 +0545

    auth_datasource

--- lib/features/auth/data/datasources/auth_datasource.dart
+++ lib/features/auth/data/datasources/auth_datasource.dart
@@ -1,1 +1,8 @@
+import 'package:bin_buddy/features/auth/data/models/auth_hive_model.dart';
 
+abstract interface class IAuthDatasource {
+  Future<bool> register(AuthHiveModel model);
+  Future<AuthHiveModel?> login(String email, String password);
+  Future<AuthHiveModel?> getCurrentUser();
+  Future<bool> logout();
+}
