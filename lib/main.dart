a1c37f6144f5f62cc49f14b0795d130fe19c7cfc
Prabin Giri <prabin17np@gmail.com>
2026-01-15 21:27:21 +0545

    adding general datasource

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,20 @@ import 'package:bin_buddy/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:bin_buddy/features/splash/presentation/pages/splash_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:bin_buddy/core/services/storage/user_session_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await HiveService().init();
-  runApp(const ProviderScope(child: MyApp()));
+  final sharedPref = await SharedPreferences.getInstance();
+
+  runApp(
+    ProviderScope(
+      overrides: [sharedPreferenceProvider.overrideWithValue(sharedPref)],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
